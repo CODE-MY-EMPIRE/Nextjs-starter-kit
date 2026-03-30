14b37b4af41c9b52a752fcf410e197265d2eab8f
Catherine Vuthi <vuthi.esther@gmail.com>
2026-03-30 19:11:08 +0300

    Update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,18 +7,6 @@ import Container from '@/components/ui/Container';
 import FooterMultiColumn from '@/components/layout/Footer/FooterMultiColumn';
 import HeroMobileApp from '@/components/sections/hero/HeroMobileApp';
 import FeaturesSection from '@/components/sections/features/FeatureSection';
-import { featuresData, featuresSplitData } from '@/data/features';
-import {
-	heroCenteredData,
-	heroDefaultData,
-	heroFeaturesData,
-	heroMobileAppData,
-} from '@/data/hero';
-import FeaturesCentered from '@/components/sections/features/FeaturesCentered';
-// import FeaturesSplit from '@/components/sections/features/FeaturesSplit';
-// import HeroFeatures from '@/components/sections/hero/HeroFeatures';
-// import HeroCentered from '@/components/sections/hero/HeroCentered';
-// import Hero from '@/components/sections/hero/Hero';
 
 export default function Home() {
 	return (
@@ -26,17 +14,12 @@ export default function Home() {
 			{/* Navbar */}
 			<NavbarCTA />
 			{/* Hero Section */}
-			<HeroMobileApp {...heroMobileAppData} />
-			{/* <HeroFeatures {...heroFeaturesData} />
-			<HeroCentered {...heroCenteredData} />
-			<Hero {...heroDefaultData} /> */}
+			<HeroMobileApp />
 
 			<main className="flex-1 py-16 space-y-16">
 				<Container>
 					{/* Features Section */}
-					<FeaturesSection features={featuresData} />
-					{/* <FeaturesSplit features={featuresSplitData} /> */}
-					{/* <FeaturesCentered features={featuresData} /> */}
+					<FeaturesSection />
 				</Container>
 			</main>
 
