0e7c8f7ead9def48679b327f3244864b4d326c66
Catherine Vuthi <vuthi.esther@gmail.com>
2026-04-04 20:39:40 +0300

    Add testimonial section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,12 @@
 'use client';
 
-import Image from 'next/image';
-
 import NavbarCTA from '@/components/layout/Navbar/NavbarCTA';
 import Container from '@/components/ui/Container';
 import FooterMultiColumn from '@/components/layout/Footer/FooterMultiColumn';
 import HeroMobileApp from '@/components/sections/hero/HeroMobileApp';
 import FeaturesSection from '@/components/sections/features/FeatureSection';
 import Pricing from '@/components/sections/pricing/Pricing';
+import { TestimonialsGrid } from '@/components/sections/testimonials/TestimonialsGrid';
 
 export default function Home() {
 	return (
@@ -22,6 +21,7 @@ export default function Home() {
 					{/* Features Section */}
 					<FeaturesSection />
 					<Pricing />
+					<TestimonialsGrid />
 				</Container>
 			</main>
 
