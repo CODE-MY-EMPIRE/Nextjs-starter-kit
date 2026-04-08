6c4f91dfe1a96e3ccb59a9b1ca8a8903a5ed7e86
Catherine Vuthi <vuthi.esther@gmail.com>
2026-04-08 21:13:02 +0300

    Add how it works component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,13 @@ import NavbarCTA from '@/components/layout/Navbar/NavbarCTA';
 import Container from '@/components/ui/Container';
 import FooterMultiColumn from '@/components/layout/Footer/FooterMultiColumn';
 import HeroMobileApp from '@/components/sections/hero/HeroMobileApp';
-import FeaturesSection from '@/components/sections/features/FeatureSection';
+import FeaturesSplit from '@/components/sections/features/FeaturesSplit';
 import Pricing from '@/components/sections/pricing/Pricing';
 import { TestimonialCarousel } from '@/components/sections/testimonials/TestimonialCarousel';
 import { PartnerMarquee } from '@/components/sections/partners/PartnerMarquee';
 import { Faq } from '@/components/sections/faq/Faq';
 import CTA from '@/components/ui/CTA';
+import HowItWorks from '@/components/sections/how-it-works/HowItWorks';
 
 export default function Home() {
 	return (
@@ -19,7 +20,8 @@ export default function Home() {
 			<main className="flex-1 py-16 space-y-16">
 				<Container>
 					<PartnerMarquee />
-					<FeaturesSection />
+					<FeaturesSplit />
+					<HowItWorks />
 					<Pricing />
 					<CTA />
 					<TestimonialCarousel />
